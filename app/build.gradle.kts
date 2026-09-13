0fad5b6e906ffe43d86d69f525b9d96ba8f0afc1
rahul01112007-sudo <rahul01112007@gmail.com>
2026-09-13 12:01:50 +0530

    Set Java and Kotlin compatibility to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,14 @@ plugins {
 android {
     namespace = "com.todo.assistant"
     compileSdk = 35
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
+kotlinOptions {
+    jvmTarget = "17"
+}
     defaultConfig {
         applicationId = "com.todo.assistant"
         minSdk = 26

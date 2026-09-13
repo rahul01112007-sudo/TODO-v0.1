bb7aeebd02721ac494bc7593b3c48cf6ed8c05f5
rahul01112007-sudo <rahul01112007@gmail.com>
2026-09-13 12:44:44 +0530

    Add new dependencies for lifecycle and coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     debugImplementation("androidx.compose.ui:ui-tooling")
+implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+implementation("com.google.mediapipe:tasks-genai:0.10.25")
 }
